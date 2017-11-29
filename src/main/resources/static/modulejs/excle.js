83bb4655df730ea47aa58addb1075c506cb50d28
lichuanzhen <czli@vcread.com>
2017-11-29 16:28:07 +0800

    更新

--- src/main/resources/static/modulejs/excle.js
+++ src/main/resources/static/modulejs/excle.js
@@ -1,11 +1,36 @@
 $(function(){
-    $(".content ul li").click(function(){
+    $("#mulu").on("click","li",function(){
         $(this).addClass("bg").siblings().removeClass("bg");
         var title = $(this).find(".title").text();
         console.log(title)
     })
-    console.log(localStorage.getItem("name"))
+
     $(".upload").click(function(){
         $(".fileUpload").addClass("show");
+    })
+    //数据回显
+    //console.log(localStorage.getItem("name"))
+    var excleUrl = "/file/show?fileName="+localStorage.getItem("name")
+    $.ajax({
+        type: 'GET',
+        url: excleUrl,
+        dataType: 'json',
+        data: '',
+        success: function (data) {
+            if (data.code == 200) {
+                var arr = data.data.fileName ;
+                if(arr){
+                    $.each(arr, function (index, item) {
+                        $("#mulu").append($('<li/>')
+                            .append($('<span class="pic"></span>'))
+                            .append($('<span class="title"></span>').html(item))
+                        );
+                    })
+                }
+
+            } else {
+                return false;
+            }
+        }
     })
 })
--- src/main/resources/static/modulejs/folder.js
+++ src/main/resources/static/modulejs/folder.js
@@ -1,10 +1,17 @@
 $(function(){
     var spExcle = "/file/show?fileName="
+    var flag = ""
     $("#mulu ").on("click","li",function(){
         $(this).addClass("bg").siblings().removeClass("bg");
         var title = $(this).find(".title").text();
         localStorage.setItem("name",title);
-        $("#ex").show().attr("href","/excle");
+        console.log(flag)
+        if(!flag){
+            var exUrl = "/excle_sp"
+        }else{
+            var exUrl = "/excle"
+        }
+        $("#ex").show().attr("href",exUrl);
         $("#oldex").hide();
         console.log(title)
     })
@@ -18,6 +25,7 @@ $(function(){
         success: function (data) {
             if (data.code == 200) {
                 var arr = data.data.fileName ;
+                flag = data.data.flag;
                 $.each(arr, function (index, item) {
                     $("#mulu").append($('<li/>')
                         .append($('<span class="pic"></span>'))
